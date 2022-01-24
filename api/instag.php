3484d0bbcae3ef698d7dbbef01bdf9b49f07568b
chris92vr <65112255+chris92vr@users.noreply.github.com>
2022-01-24 22:21:00 +0100

    Initial commit

--- api/instag.php
+++ api/instag.php
@@ -1,29 +1,16 @@
-<?php
+var axios = require("axios").default;
 
-$curl = curl_init();
+var options = {
+  method: 'GET',
+  url: 'https://instagram29.p.rapidapi.com/search/avtistkid',
+  headers: {
+    'x-rapidapi-host': 'instagram29.p.rapidapi.com',
+    'x-rapidapi-key': '251669fb4bmshce01bc256be2467p164627jsnefa56c023a18'
+  }
+};
 
-curl_setopt_array($curl, [
-	CURLOPT_URL => "https://instagram28.p.rapidapi.com/user_info?user_name=avtistkid",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"x-rapidapi-host: instagram28.p.rapidapi.com",
-		"x-rapidapi-key: 251669fb4bmshce01bc256be2467p164627jsnefa56c023a18"
-	],
-]);
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-	echo "cURL Error #:" . $err;
-} else {
-	echo $response;
-}
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
