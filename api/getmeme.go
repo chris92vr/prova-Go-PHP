55d97403794470809ae695d3118e85925691c941
chris92vr <65112255+chris92vr@users.noreply.github.com>
2022-06-28 17:49:21 +0200

    Get Url

--- api/getmeme.go
+++ api/getmeme.go
@@ -29,3 +29,24 @@ func GetMemeTemplate(w http.ResponseWriter, r *http.Request) {
 	}
 	return
 }
+
+func GetMeme(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]string)
+	getMemeApi := "https://dog.ceo/api/breeds/image/random"
+	getMemeResponse, err := http.Get(getMemeApi)
+	getMemeBody, err := ioutil.ReadAll(getMemeResponse.Body)
+	getMemeJson := string(getMemeBody)
+	fmt.Println("GetMeme API Json String:", getMemeJson)
+	var getMeme map[string]interface{}
+	json.Unmarshal([]byte(getMemeBody), &getMeme)
+	resp["message"] = fmt.Sprint(getMeme["message"])
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Println("Error happened in JSON marshal. Err: %s", err)
+	} else {
+		w.Write(jsonResp)
+	}
+	return
+}
